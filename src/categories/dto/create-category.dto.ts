15c858e3a5c00789f3b22cb3df515fce4395cf15
Jorge Rodrigo <86239864+Jorge-Rodrigo@users.noreply.github.com>
2023-08-20 13:06:01 -0300

    feat: data validators

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,4 +1,12 @@
+import { IsNotEmpty, IsString, MaxLength, IsOptional } from 'class-validator';
+
 export class CreateCategoryDto {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsOptional()
   description?: string | null;
 }
